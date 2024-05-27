56960179b29d8723a9c29cddccd891b508960f23
Jaspreet <jaspreetmehra185@gmail.com>
2024-05-27 22:23:57 +0530

    create project

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import NavBar from "@/components/navigationBar/NavBar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar/>
+    </div>
+  );
+}
